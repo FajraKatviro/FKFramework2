e07745a32550bb4a3da7bf36b7b98f03b7c8f979
Fajra Katviro <pahaa7@mail.ru>
2015-10-14 10:52:40 +0300

    removed test qt extra dependency

--- FKTests/ConsoleAppTest/run.bat
+++ FKTests/ConsoleAppTest/run.bat
@@ -1,7 +1,8 @@
 @echo off
+cd "../../release/bin_win"
 setlocal enabledelayedexpansion
 set string=
 FOR /F "tokens=1" %%I IN (testList.txt) DO (
 	set string=!string! %%I
 )
-"../../tests/bin/ConsoleAppTest.exe" -o "result.txt" !string!
+"../../tests/bin/ConsoleAppTest.exe" -o "../../FKTests/ConsoleAppTest/result.txt" !string!
